6a4c5243fa6c92ad9717bf4b6aaeaa35c3412ee9
nwesthoff <nilsepils94@hotmail.com>
2017-10-13 18:15:34 +0200

    create fair page

---
+++ page-fair.php
@@ -0,0 +1,41 @@
+<?php
+  /**
+  * Template Name: Fair Page
+  */
+
+  get_header();
+?>
+
+  <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+    <main class="fair__top">
+    	<h1 class="fair__title"><span><?php the_title(); ?></span></h1>
+    </main>
+
+    <div class="section">
+      <div class="section__wrapper">
+        <p class="fair__descr"><?php echo get_the_content(); ?></p>
+      </div>
+    </div>
+  <?php endwhile; endif; ?>
+
+  <div class="section__title__wrapper">
+    <h2 class="section__title"><span>Program</span></h2>
+  </div>
+  <div class="section">
+    <div class="section__wrapper">
+
+    </div>
+  </div>
+
+  <div class="section__title__wrapper">
+    <h2 class="section__title"><span>Map</span></h2>
+  </div>
+  <div class="section">
+    <div class="section__wrapper">
+
+    </div>
+  </div>
+
+<?php
+  get_footer();
+?>
