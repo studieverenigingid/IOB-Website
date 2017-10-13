aebd93f23f6d945ef5fd01070de1a43d4aa66630
nwesthoff <nilsepils94@hotmail.com>
2017-10-13 21:10:36 +0200

    adds company-custom-fields.php

---
+++ inc/company-custom-fields.php
@@ -0,0 +1,47 @@
+<?php
+  if( function_exists('acf_add_local_field_group') ):
+
+  acf_add_local_field_group(array (
+  	'key' => 'group_59e0ed7815181',
+  	'title' => 'Company Top Fields',
+  	'fields' => array (
+  		array (
+  			'key' => 'field_59e0eda0f997e',
+  			'label' => 'Company Type',
+  			'name' => 'company_type',
+  			'type' => 'text',
+  			'instructions' => 'Set a short description for the company',
+  			'required' => 1,
+  			'conditional_logic' => 0,
+  			'wrapper' => array (
+  				'width' => '',
+  				'class' => '',
+  				'id' => '',
+  			),
+  			'default_value' => '',
+  			'placeholder' => '',
+  			'prepend' => '',
+  			'append' => '',
+  			'maxlength' => 44,
+  		),
+  	),
+  	'location' => array (
+  		array (
+  			array (
+  				'param' => 'post_type',
+  				'operator' => '==',
+  				'value' => 'company',
+  			),
+  		),
+  	),
+  	'menu_order' => 0,
+  	'position' => 'acf_after_title',
+  	'style' => 'default',
+  	'label_placement' => 'top',
+  	'instruction_placement' => 'label',
+  	'hide_on_screen' => '',
+  	'active' => 1,
+  	'description' => '',
+  ));
+
+  endif;
