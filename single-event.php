8fdbfd59efe630cf86f2e0288fe19b278ddc2228
nwesthoff <nilsepils94@hotmail.com>
2017-10-13 22:45:43 +0200

    adds single-event.php and needed styling

---
+++ single-event.php
@@ -0,0 +1,56 @@
+<?php
+  get_header();
+  if(have_posts()) : while(have_posts()) : the_post();
+?>
+
+  <main class="event--single__top">
+    <div class="section__wrapper">
+      <h1 class="event--single__title"><span><?= the_title(); ?></span></h1>
+
+      <?php
+        $start = new DateTime(get_field('start_datetime'));
+        $start->setTimezone( new DateTimeZone('Europe/Amsterdam') );
+        $end = new DateTime(get_field('end_datetime'));
+        $end->setTimezone( new DateTimeZone('Europe/Amsterdam') );
+        $start_month = $start->format('F');
+        $start_day   = $start->format('jS');
+        $end_month = $end->format('F');
+        $end_day   = $end->format('jS');
+        $start_time = $start->format('H:i');
+        $end_time   = $end->format('H:i');
+        $location_name = get_field('location_name');
+      ?>
+
+      <div class="event--single__meta">
+				<?php
+					echo $start_month . ' ' . $start_day . ', '. $start_time . ' – ';
+					if ($start_day != $end_day){
+						echo $end_month . ' ' . $end_day . ', ' . $end_time;
+					} else {
+						echo $end_time;
+					}
+					echo ($location_name) ? ' @ ' . $location_name : '';
+				?>
+			</div>
+
+      <?php if ( has_post_thumbnail() ) : ?>
+  				<div class="event--single__thumb">
+  					<?php
+  					the_post_thumbnail(
+  						'large',
+  						array('class' => 'event--single__img')
+  					);
+  					?>
+  				</div>
+  			<?php endif; ?>
+      </div>
+  </main>
+
+  <div class="event--single__wrapper">
+    <?php the_content(); ?>
+  </div>
+
+<?php
+  endwhile; endif;
+  get_footer();
+?>
