ac49f1c1c9458e309c3988874765ff0aa162acaf
nwesthoff <nilsepils94@hotmail.com>
2017-10-13 22:13:42 +0200

    event archive shows upcoming and past events

--- archive-event.php
+++ archive-event.php
@@ -10,12 +10,29 @@
   </main>
 
   <?php
-    if ( have_posts() ) {
+    $today = date('Ymd');
+    $upcoming_loop = new WP_Query( array(
+    'post_type' => 'event',
+    'posts_per_page' => 6,
+    'meta_query' => array(
+    array(
+      'key'     => 'start_datetime',
+      'compare' => '>=',
+      'value'   => $today,
+      'type'    => 'DATE'
+    ),
+    ),
+    'orderby' => 'start_datetime',
+    'order' => 'ASC',
+    ) );
+    if ($upcoming_loop->have_posts()) {
   ?>
-
+    <div class="section__title__wrapper">
+      <h2 class="section__title"><span>Upcoming Events</span></h2>
+    </div>
     <div class="section">
       <div class="section__wrapper">
-        <?php while(have_posts()) : the_post(); ?>
+        <?php while($upcoming_loop->have_posts()) : $upcoming_loop->the_post(); ?>
           <?php include 'inc/event-small.php';?>
         <?php endwhile;?>
       </div>
@@ -25,6 +42,40 @@
       }
       wp_reset_postdata();
     ?>
+
+    <?php
+      $today = date('Ymd');
+      $past_loop = new WP_Query( array(
+      'post_type' => 'event',
+      'posts_per_page' => 6,
+      'meta_query' => array(
+      array(
+        'key'     => 'start_datetime',
+        'compare' => '<',
+        'value'   => $today,
+        'type'    => 'DATE'
+      ),
+      ),
+      'orderby' => 'start_datetime',
+      'order' => 'DESC',
+      ) );
+      if ($past_loop->have_posts()) {
+    ?>
+      <div class="section__title__wrapper">
+        <h2 class="section__title"><span>Past Events</span></h2>
+      </div>
+      <div class="section">
+        <div class="section__wrapper">
+          <?php while($past_loop->have_posts()) : $past_loop->the_post(); ?>
+            <?php include 'inc/event-small.php';?>
+          <?php endwhile;?>
+        </div>
+      </div>
+
+      <?php
+        }
+        wp_reset_postdata();
+      ?>
 
 <?php
   get_footer();
