551204bd6dd5ff464a86c6cd63ca8a0611ec47ad
nwesthoff <nilsepils94@hotmail.com>
2017-10-13 12:01:55 +0200

    adds company and event styling on front-page

--- front-page.php
+++ front-page.php
@@ -18,8 +18,8 @@
        	// loop through the rows of data
           while ( have_rows('stats_repeat_group') ) : the_row();?>
               <div class="section--stats__group">
-                <h3><?=the_sub_field('stat_value');?>+</h3>
-                <h4><?=the_sub_field('stat_description');?></h4>
+                <h3 class="stat__value"><?=the_sub_field('stat_value');?>+</h3>
+                <h4 class="stat__description"><?=the_sub_field('stat_description');?></h4>
               </div>
 
           <?php endwhile;
@@ -47,14 +47,17 @@
     <h2 class="section__title"><span>The Companies</span></h2>
   </div>
   <div class="section">
-    <div class="section__wrapper">
+    <div class="section__wrapper section__wrapper--companies">
       <?php
       $args = array( 'post_type' => 'company', 'posts_per_page' => 10, 'orderby' => 'rand' );
       $loop = new WP_Query( $args );
 
       while ( $loop->have_posts() ) : $loop->the_post(); ?>
-        <h3><?= the_title(); ?></h3>
-        <?= the_post_thumbnail('medium'); ?>
+        <div class="company--small">
+          <a href="<?= the_permalink();?>">
+            <?= the_post_thumbnail('medium'); ?>
+          </a>
+        </div>
       <?php endwhile; ?>
     </div>
   </div>
@@ -69,33 +72,37 @@
       $loop = new WP_Query( $args );
 
       while ( $loop->have_posts() ) : $loop->the_post(); ?>
-        <h3><?= the_title(); ?></h3>
-        <?= the_post_thumbnail('medium'); ?>
+        <div class="event--small">
+          <a href="<?= the_permalink();?>">
+            <h3><?= the_title(); ?></h3>
+            <?= the_post_thumbnail('medium'); ?>
 
-        <?php
-  				$start = new DateTime(get_field('start_datetime'));
-  				$start->setTimezone( new DateTimeZone('Europe/Amsterdam') );
-  				$end = new DateTime(get_field('end_datetime'));
-  				$end->setTimezone( new DateTimeZone('Europe/Amsterdam') );
-  				$start_month = $start->format('F');
-  				$start_day   = $start->format('jS');
-  				$end_month = $end->format('F');
-  				$end_day   = $end->format('jS');
-  				$start_time = $start->format('H:i');
-  				$end_time   = $end->format('H:i');
-  				$location_name = get_field('location_name');
-  			?>
+            <?php
+      				$start = new DateTime(get_field('start_datetime'));
+      				$start->setTimezone( new DateTimeZone('Europe/Amsterdam') );
+      				$end = new DateTime(get_field('end_datetime'));
+      				$end->setTimezone( new DateTimeZone('Europe/Amsterdam') );
+      				$start_month = $start->format('F');
+      				$start_day   = $start->format('jS');
+      				$end_month = $end->format('F');
+      				$end_day   = $end->format('jS');
+      				$start_time = $start->format('H:i');
+      				$end_time   = $end->format('H:i');
+      				$location_name = get_field('location_name');
+      			?>
 
-        <?php
-  					echo $start_month . ' ' . $start_day . ', '. $start_time . ' – ';
-  					if ($start_day != $end_day){
-  						echo $end_month . ' ' . $end_day . ', ' . $end_time;
-  					} else {
-  						echo $end_time;
-  					}
-  					echo ($location_name) ? ' @ ' . $location_name : '';
-  				?>
-        <p><?= the_excerpt(); ?></p>
+            <?php
+      					echo $start_month . ' ' . $start_day . ', '. $start_time . ' – ';
+      					if ($start_day != $end_day){
+      						echo $end_month . ' ' . $end_day . ', ' . $end_time;
+      					} else {
+      						echo $end_time;
+      					}
+      					echo ($location_name) ? ' @ ' . $location_name : '';
+      				?>
+            <p><?= the_excerpt(); ?></p>
+          </a>
+        </div>
       <?php endwhile; ?>
     </div>
   </div>
