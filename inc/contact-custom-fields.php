b55e624d238a8526d28eb619f2b99a831f832325
nwesthoff <nilsepils94@hotmail.com>
2017-10-18 17:04:27 +0200

    adds contact-custom-fields.php

---
+++ inc/contact-custom-fields.php
@@ -0,0 +1,47 @@
+<?php
+  if( function_exists('acf_add_local_field_group') ):
+
+  acf_add_local_field_group(array (
+  'key' => 'group_59e75d8a053ad',
+  'title' => 'Committee Info',
+  'fields' => array (
+    array (
+      'key' => 'field_59e75d98f5a5b',
+      'label' => 'Committee Info',
+      'name' => 'committee_info',
+      'type' => 'wysiwyg',
+      'instructions' => 'Set the committee info',
+      'required' => 1,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'default_value' => '',
+      'tabs' => 'all',
+      'toolbar' => 'full',
+      'media_upload' => 1,
+      'delay' => 0,
+    ),
+  ),
+  'location' => array (
+    array (
+      array (
+        'param' => 'page_template',
+        'operator' => '==',
+        'value' => 'page-contact.php',
+      ),
+    ),
+  ),
+  'menu_order' => 0,
+  'position' => 'normal',
+  'style' => 'default',
+  'label_placement' => 'top',
+  'instruction_placement' => 'label',
+  'hide_on_screen' => '',
+  'active' => 1,
+  'description' => '',
+  ));
+
+  endif;
