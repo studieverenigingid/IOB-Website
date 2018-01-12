94784524780652d3f1e57fcff18598b0e6abf6b7
nwesthoff <nilsepils94@hotmail.com>
2018-01-12 16:02:56 +0100

    fixes event-archive.php not showing first and last item

--- archive-event.php
+++ archive-event.php
@@ -1,64 +1,61 @@
 <?php
-	get_header();
+get_header();
 ?>
-	<main>
-		<div class="event--archive__top">
-			<h1 class="event--archive__title"><span><?= post_type_archive_title(); ?></span></h1>
-		</div>
+<main>
+	<div class="event--archive__top">
+		<h1 class="event--archive__title"><span><?= post_type_archive_title(); ?></span></h1>
+	</div>
 
-		<div class="events">
+	<div class="events">
 
-			<?php
-				$current_date = new DateTime();
-				$current_date->setTimezone( new DateTimeZone('Europe/Amsterdam') );
-				$first_past = true;
-				$post_no = 0;
-			?>
+		<?php
+		$current_date = new DateTime();
+		$current_date->setTimezone( new DateTimeZone('Europe/Amsterdam') );
+		$first_past = true;
+		$post_no = 0;
+		?>
 
-			<?php if(have_posts()) : the_post(); ?>
-					<?php while(have_posts()) : the_post(); ?>
-
-						<?php
-							$start = new DateTime(get_field('start_datetime'));
-							$start->setTimezone( new DateTimeZone('Europe/Amsterdam') );
-							if ($start >= $current_date && $first_past && $post_no === 0):
-						?>
+		<?php if(have_posts()){?>
 			<div class="section">
-				<div class="section__title__wrapper">
-					<h2 class="section__title"><span>Upcoming Events</span></h2>
-				</div>
-				<div class="section__wrapper">
+				<?php while(have_posts()) : the_post(); ?>
+
 					<?php
+					$start = new DateTime(get_field('start_datetime'));
+					$start->setTimezone( new DateTimeZone('Europe/Amsterdam') );
+					if ($start >= $current_date && $first_past && $post_no === 0):
+						?>
+						<div class="section__title__wrapper">
+							<h2 class="section__title"><span>Upcoming Events</span></h2>
+						</div>
+						<div class="section__wrapper">
+							<?php
 						endif;
 						if ($start < $current_date && $first_past):
 							$first_past = false;
-					?>
-				</div>
-			</div>
+							?>
+						</div>
 
-			<div class="section">
-				<div class="section__title__wrapper">
-					<h2 class="section__title"><span>Past Events</span></h2>
-				</div>
-				<div class="section__wrapper">
+						<div class="section__title__wrapper">
+							<h2 class="section__title"><span>Past Events</span></h2>
+						</div>
+						<div class="section__wrapper">
 						<?php endif; ?>
 
 						<?php
-							// Add an order to upcoming events to sort them reverse from how we got them
-							if ($start > $current_date) {
-								$order_override = "style='order: -$post_no'";
-								$post_no++;
-							} else {
-								$order_override = '';
-							}
+						// Add an order to upcoming events to sort them reverse from how we got them
+						if ($start > $current_date) {
+							$order_override = "style='order: -$post_no'";
+							$post_no++;
+						} else {
+							$order_override = '';
+						}
 						?>
 
 						<?php include( 'inc/event-small.php' ); ?>
 
 					<?php endwhile; ?>
 				</div>
-			</div>
-			<?php endif; ?>
+			<?php }; ?>
 		</div>
 	</main>
 
@@ -67,74 +64,7 @@
 			<?php include 'inc/pagination.php'; ?>
 		</div>
 	</div>
-	<?php /*
-		$today = date('Ymd');
-		$upcoming_loop = new WP_Query( array(
-		'post_type' => 'event',
-		'posts_per_page' => 6,
-		'meta_query' => array(
-		array(
-			'key'     => 'start_datetime',
-			'compare' => '>=',
-			'value'   => $today,
-			'type'    => 'DATE'
-		),
-		),
-		'orderby' => 'start_datetime',
-		'order' => 'ASC',
-		) );
-		if ($upcoming_loop->have_posts()) {
-	?>
-		<div class="section__title__wrapper">
-			<h2 class="section__title"><span>Upcoming Events</span></h2>
-		</div>
-		<div class="section">
-			<div class="section__wrapper">
-				<?php while($upcoming_loop->have_posts()) : $upcoming_loop->the_post(); ?>
-					<?php include 'inc/event-small.php';?>
-				<?php endwhile;?>
-			</div>
-		</div>
-
-		<?php
-			}
-			wp_reset_postdata();
-		?>
-
-		<?php
-			$today = date('Ymd');
-			$past_loop = new WP_Query( array(
-			'post_type' => 'event',
-			'posts_per_page' => 6,
-			'meta_query' => array(
-			array(
-				'key'     => 'start_datetime',
-				'compare' => '<',
-				'value'   => $today,
-				'type'    => 'DATE'
-			),
-			),
-			'orderby' => 'start_datetime',
-			'order' => 'DESC',
-			) );
-			if ($past_loop->have_posts()) {
-		?>
-			<div class="section__title__wrapper">
-				<h2 class="section__title"><span>Past Events</span></h2>
-			</div>
-			<div class="section">
-				<div class="section__wrapper">
-					<?php while($past_loop->have_posts()) : $past_loop->the_post(); ?>
-						<?php include 'inc/event-small.php';?>
-					<?php endwhile;?>
-				</div>
-			</div>
-
-			<?php
-				}
-				wp_reset_postdata();
-			*/ ?>
 
 <?php
-	get_footer();
+get_footer();
 ?>
