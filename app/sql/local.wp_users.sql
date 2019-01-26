7187c8e192e8dcf77a08ebe408e22eb05d05a173
Floris Jansen <florismartijnjansen@gmail.com>
2019-01-26 18:42:29 +0100

    Revert "intialisation"
    This reverts commit c881ef74706ba2f075cfda187d4510c1322288ad.

--- app/sql/local.wp_users.sql
+++
@@ -1,5 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `wp_users` VALUES
-(1,"elias","$P$BXsQ9ycsMhzZ5qIvCcR6HRZmMTMhiP0","elias","eliaslinden@gmail.com","","2018-12-28 15:44:13","",0,"elias");
