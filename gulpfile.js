ce4be9287f75b7f8080c9dce6d661f6f2a784494
Floris Jansen <florismartijnjansen@gmail.com>
2022-02-16 15:54:56 +0100

    upgrade node packages and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@
 var gulp = require('gulp');
 
 // Include plugins
-var gutil = require('gulp-util');
+var log = require('fancy-log');
+var colors = require('ansi-colors');
 
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var terser = require('gulp-terser');
 
 var plumber = require('gulp-plumber');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var sourcemaps = require('gulp-sourcemaps');
 var prefix = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
@@ -23,57 +24,57 @@ var src = 'src/';
 var dest = 'static/';
 
  // Concatenate & Minify JS
-gulp.task('scripts', function() {
-		return gulp.src(src + 'js/*.js')
-				.pipe(plumber(function(error) {
-						gutil.log(gutil.colors.red(error.message));
-						this.emit('end');
-				}))
-				.pipe(concat('main.js'))
-				.pipe(uglify())
-				.pipe(gulp.dest(dest + 'js'));
+ gulp.task('scripts', function() {
+	return gulp.src(src + 'js/*.js')
+			.pipe(plumber(function(error) {
+					log(colors.red(error.message));
+					this.emit('end');
+			}))
+			.pipe(concat('main.js'))
+			.pipe(terser())
+			.pipe(gulp.dest(dest + 'js'));
 });
 
 gulp.task('scriptsDev', function() {
-		return gulp.src(src + 'js/*.js')
-				.pipe(plumber(function(error) {
-						gutil.log(gutil.colors.red(error.message));
-						this.emit('end');
-				}))
-				.pipe(sourcemaps.init())
-				.pipe(concat('main.js'))
-				.pipe(sourcemaps.write())
-				.pipe(gulp.dest(dest + 'js'));
+	return gulp.src(src + 'js/*.js')
+			.pipe(plumber(function(error) {
+					log(colors.red(error.message));
+					this.emit('end');
+			}))
+			.pipe(sourcemaps.init())
+			.pipe(concat('main.js'))
+			.pipe(sourcemaps.write())
+			.pipe(gulp.dest(dest + 'js'))
 });
 
 gulp.task('sass', function() {
-		return gulp.src(src + 'scss/base.scss')
-				.pipe(plumber(function(error) {
-						gutil.log(gutil.colors.red(error.message));
-						this.emit('end');
-				}))
-				.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-				.pipe(prefix({browsers: ['last 4 version']}))
-				.pipe(rename('main.css'))
-				.pipe(gulp.dest(dest + 'css'));
+	return gulp.src(src + 'scss/base.scss')
+			.pipe(plumber(function(error) {
+					log(colors.red(error.message));
+					this.emit('end');
+			}))
+			.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+			.pipe(prefix())
+			.pipe(rename('main.css'))
+			.pipe(gulp.dest(dest + 'css'));
 });
 
 gulp.task('sassDev', function() {
-		return gulp.src(src + 'scss/base.scss')
-				.pipe(plumber(function(error) {
-						gutil.log(gutil.colors.red(error.message));
-						this.emit('end');
-				}))
-				.pipe(sourcemaps.init())
-				.pipe(sass().on('error', sass.logError))
-				.pipe(sourcemaps.write())
-				.pipe(rename('main.css'))
-				.pipe(gulp.dest(dest + 'css'));
+	return gulp.src(src + 'scss/base.scss')
+			.pipe(plumber(function(error) {
+					log(colors.red(error.message));
+					this.emit('end');
+			}))
+			.pipe(sourcemaps.init())
+			.pipe(sass().on('error', sass.logError))
+			.pipe(sourcemaps.write())
+			.pipe(rename('main.css'))
+			.pipe(gulp.dest(dest + 'css'))
 });
 
 gulp.task('copy-scss', function() {
-		return gulp.src(src + 'scss/*.scss')
-				.pipe(gulp.dest(dest + 'css'));
+	return gulp.src(src + 'scss/*.scss')
+			.pipe(gulp.dest(dest + 'css'));
 });
 
 gulp.task('images', function() {
@@ -100,12 +101,12 @@ gulp.task('watch', function() {
 
 
 // Default task
-gulp.task('default', ['scriptsDev', 'sassDev', 'copy-scss', 'images', 'fonts', 'watch']);
+gulp.task('default', gulp.series('scriptsDev', 'sassDev', 'copy-scss', 'images', 'fonts', 'watch'));
 // Build task
-gulp.task('build', ['scripts', 'sass', 'images', 'fonts']);
+gulp.task('build', gulp.parallel('scripts', 'sass', 'images', 'fonts'));
 
 gulp.task('clear', function() {
 	// Still pass the files to clear cache for
-	gulp.src('images/**')
+	return gulp.src('images/**')
 		.pipe(cache.clear());
 });
